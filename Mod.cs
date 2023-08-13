78c32513a438878d15f1dee40489ce0dca6c895a
samtabareh <sam.tabareh@gmail.com>
2023-08-14 00:30:00 +0330

    No Build :'(

--- Mod.cs
+++ Mod.cs
@@ -10,21 +10,7 @@ namespace ExoticNS
         public override void Ready()
         {
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "exotic_blueprint_pineapple", 1);
-            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.BasicFood, "exotic_pineapple", 1);
-        }
-        private void Awake()
-        {
-            this.Harmony.PatchAll(typeof(Patches));
-        }
-    }
-
-    public static class Patches
-    {
-        [HarmonyPatch(typeof(BlueprintGrowth), "PopulateSubprints")]
-        [HarmonyPrefix]
-        public static void stuff(List<Growable> ___growables)
-        {
-            ___growables.Add(new Growable("pineapple", "exotic_pineapple_growth_status_", "pineapple", 2, 1f));
+            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedFood, "exotic_pineapple", 1);
         }
     }
 }
