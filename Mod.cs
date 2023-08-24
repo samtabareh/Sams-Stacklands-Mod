727c6e2589cecff2b52f6a5756adcb2cac8bd464
samtabareh <sam.tabareh@gmail.com>
2023-08-24 12:48:19 +0330

    Blueprint Growth Patch, yay

--- Mod.cs
+++ Mod.cs
@@ -7,6 +7,16 @@ namespace ExoticNS
 {
     public class Exotic : Mod
     {
+        private static Harmony harmony;
+
+        [HarmonyPatch(typeof(BlueprintGrowth), "Init")]
+        [HarmonyPostfix]
+        public static void BlueprintGrowth__Init_Postfix(BlueprintGrowth forest)
+        {
+            growables.Add(new Growable("exotic_mango", "exotic_idea_mango_grow", "exotic_mango", 2, 120f));
+            growables.Add(new Growable("exotic_pineapple", "exotic_idea_pineapple_grow", "exotic_pineapple", 2, 120f));
+            growables.Add(new Growable("exotic_truffle", "exotic_idea_alive_truffle_status", "exotic_alive_truffle", 2, 120f));
+        }
         public override void Ready()
         {
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "exotic_blueprint_luxury_fruit_salad", 1);
@@ -19,6 +29,8 @@ namespace ExoticNS
             bag.Chances.Add(new CardChance("exotic_alive_truffle", 1));
             CardData bone = WorldManager.instance.GetCardPrefab("bone");
             bone.descriptionOverride = "Some say it has magical properties within!";
+            harmony.PatchAll();
+            
         }
     }
 }
