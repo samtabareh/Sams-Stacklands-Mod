cf0afdc3f72bdaf88fb96546e19cf5164e49c4e7
samtabareh <sam.tabareh@gmail.com>
2023-09-27 18:28:30 +0330

    V0.0.6

--- Mod.cs
+++ Mod.cs
@@ -4,22 +4,16 @@ namespace ExoticNS
 {
     public class Exotic : Mod
     {
-        private static Harmony harmony;
-        private static List<BlueprintGrowth.Growable> growables = new List<BlueprintGrowth.Growable>();
-
-        [HarmonyPatch(typeof(BlueprintGrowth), "Init")]
-        [HarmonyPostfix]
-        public static void BlueprintGrowth__Init_Postfix(BlueprintGrowth forest)
+        [HarmonyPatch(typeof(BlueprintGrowth), "PopulateSubprints")]
+        [HarmonyPrefix]
+        public static void BlueprintGrowth__PopulateSubprints_Prefix(GameDataLoader loader, List<BlueprintGrowth.Growable> ___growables)
         {
-            growables.Add(new BlueprintGrowth.Growable("exotic_mango", "exotic_idea_mango_grow", "exotic_mango", 2, 120f));
-            growables.Add(new BlueprintGrowth.Growable("exotic_coconut", "exotic_idea_coconut_grow", "exotic_coconut_tree", 1, 120f));
-            growables.Add(new BlueprintGrowth.Growable("exotic_pineapple", "exotic_idea_pineapple_grow", "exotic_pineapple", 2, 120f));
-            growables.Add(new BlueprintGrowth.Growable("exotic_truffle", "exotic_idea_alive_truffle_status", "exotic_alive_truffle", 2, 120f));
+            ___growables.Add(new BlueprintGrowth.Growable("exotic_mango", "exotic_mango_grow", "exotic_mango", 2, 120f));
+            ___growables.Add(new BlueprintGrowth.Growable("exotic_coconut", "exotic_coconut_tree_grow", "exotic_coconut_tree", 1, 120f));
+            ___growables.Add(new BlueprintGrowth.Growable("exotic_pineapple", "exotic_pineapple_grow", "exotic_pineapple", 2, 120f));
+            ___growables.Add(new BlueprintGrowth.Growable("exotic_truffle", "exotic_idea_alive_truffle_status", "exotic_alive_truffle", 2, 120f));
         }
-        private void Awake()
-        {
-            
-        }
+        
         public override void Ready()
         {
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "exotic_blueprint_luxury_fruit_salad", 1);
@@ -27,7 +21,8 @@ namespace ExoticNS
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "exotic_blueprint_alive_truffle", 1);
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedFood, "exotic_pineapple", 1);
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedFood, "exotic_mango", 1);
-            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedFood, "exotic_coconut", 1);
+            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.Island_BasicHarvestable, "exotic_coconut_tree", 1);
+            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedFood, "exotic_coconut_tree", 1);
             
             Harvestable forest = (Harvestable)WorldManager.instance.GetCardPrefab("forest");
             CardBag bag = forest.MyCardBag;
@@ -36,7 +31,7 @@ namespace ExoticNS
             CardData bone = WorldManager.instance.GetCardPrefab("bone");
             bone.descriptionOverride = "Some say it has magical properties within!";
 
-            harmony.PatchAll();
+            Harmony.PatchAll();
         }
     }
 }
